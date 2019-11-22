226956a0a1655904c6f32c6117e042f03d3ceea3
GigaHierz <hierzilena@gmail.com>
2019-11-22 11:59:08 +0100

    setup the backend

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { Pool } from 'pg';
+
 export const getApp = () => {
   const app = express();
   app.use(bodyParser.urlencoded({ extended: false }));
@@ -8,5 +10,15 @@ export const getApp = () => {
   app.get('/api/v1/test', (_, res) => {
     res.json({ ok: true });
   });
+
+  app.get('/api/projects', (req, res) => {
+
+  })
+
+  app.post('/api/projects', (req, res) => {
+    const newProject = req.body;
+    res.send(newProject)
+  });
+
   return app;
 };
