de3f8663b15f6ae3da168deff15995967a5dcd9e
Gabriel Heinrich <gabriel@codecampleipzig.de>
2019-11-25 19:00:28 +0100

    Remove chai/mocha from dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { PORT } from "src/configuration";
+import { PORT, environment } from "src/configuration";
 import { getApp } from "src/app";
 const startServer = async () => {
   try {
     const app = await getApp();
     app.listen(PORT, () => {
-      console.log(`server started at http://localhost:${PORT}`);
+      console.log(`server started at http://localhost:${PORT} in ${environment} environment`);
     });
   } catch (error) {
     console.error(error);
