9b116979101ec30b0accc20be22425594ef1b9fd
Gabriel Heinrich <gabriel@codecampleipzig.de>
2019-11-25 22:04:16 +0100

    Move sql files to separate folder

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -4,13 +4,13 @@ import { query } from "../db";
 
 export async function setupDatabase() {
   // Init Tables
-  const databaseInitSQL = fs.readFileSync(__dirname + "/database-init.sql", { encoding: "utf8" });
+  const databaseInitSQL = fs.readFileSync(__dirname + "/../../sql/database-init.sql", { encoding: "utf8" });
   await query(databaseInitSQL);
 
   // Insert Test Data if in test environment
   if (environment == "development" || environment == "test") {
     if ((await query("SELECT * from users")).rows.length == 0) {
-      const testDataSQL = fs.readFileSync(__dirname + "/test-data.sql", { encoding: "utf8" });
+      const testDataSQL = fs.readFileSync(__dirname + "/../../sql/test-data.sql", { encoding: "utf8" });
       await query(testDataSQL);
     }
   }
