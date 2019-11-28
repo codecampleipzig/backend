6d2c7a76dd9ce4fde1e8c03db1b92675de57f077
GigaHierz <hierzilena@gmail.com>
2019-11-28 13:38:37 +0100

    Change queries in UserController form mockdata to database

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -4,7 +4,7 @@ import { query } from "../db";
 
 export const getUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const dbResponse = await query("SELECT * from users WHERE user_id = $1", [req.params.id]);
+    const dbResponse = await query(`SELECT * from users WHERE user_id = $1`, [req.params.id]);
     if (dbResponse.rows.length == 1) {
       res.send({ project: dbResponse.rows[0] });
     } else {
@@ -29,7 +29,7 @@ export const registerUser = async (req: Request, res: Response, next: NextFuncti
     }
 
     await query(
-      "INSERT INTO users(user_name, user_email, password, user_image_url, join_date) VALUES($1, $2, $3, $4, $5)",
+      `INSERT INTO users(user_name, user_email, password, user_image_url, join_date) VALUES($1, $2, $3, $4, $5)`,
       [name, email, password, imageURL, joinDate],
     );
     res.status(201).send({ status: "ok" });
@@ -38,12 +38,29 @@ export const registerUser = async (req: Request, res: Response, next: NextFuncti
   }
 };
 
-export const editUser = (req: Request, res: Response) => {
-  const id = req.params.id;
-  res.send("PUT request to the userID " + id);
+export const editUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = req.params.id;
+    const {user_name, user_email, user_image_url} =  req.body;
+    const dbResponse = await query(
+      `UPDATE users SET user_name = $1, user_email = $2, user_image_url = $3
+      WHERE user_id = $3 Returning *`, 
+      [user_name, user_email, user_image_url, id]
+      );
+      res.send({users: dbResponse.rows[0]})
+  }catch(error){
+    next(error);
+  }
 };
 
-export const deleteUser = (req: Request, res: Response) => {
-  const id = req.params.id;
-  res.send("PUT request to taskId" + id);
+export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+  try{
+    const user_id = req.params.id;
+    const dbResponse = await query (
+      `DELETE FROM users WHERE user_id = $1 RETURNING id`, [user_id]
+      );
+    res.send({user_id})
+  }catch(error) {
+    next(error);
+  }
 };
