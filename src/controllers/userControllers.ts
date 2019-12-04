e1088b4a90924ef2614318677b8a37c3755c44fb
Simona Ilievska <s.ilievska.10@gmail.com>
2019-12-04 14:06:19 +0100

    Remove comments from Register and Login

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -5,6 +5,7 @@ import { QueryResult } from "pg";
 import * as bcrypt from "bcrypt";
 import * as jwt from "jsonwebtoken";
 import { users } from "../mockdata";
+import { User } from "src/datatypes/User";
 
 // Generate private key with jwt using the following command in node:
 // require('crypto').randomBytes(64).toString('hex')
@@ -18,33 +19,32 @@ export const getUser = (req: Request, res: Response) => {
 
 export const registerUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    // take user data from req.body
+    // Take user data from req.body
     const { username, email, password } = req.body;
-    // check for value in name, email and password - required fields validation
+    // Check for value in name, email and password - required fields validation
     if (!username || !email || !password) {
       return res.status(400).send({ message: "Username, email or password required." });
     }
-    // TODO: check for expected value in name, email and password - valid data format
+    // TODO: Check for expected value in name, email and password - valid data format
     // name: string containing letters, numbers?, special characters?
     // email: standard email format validation a@a.a
     // password: Your password must contain an uppercase, a lowercase, a special character and a number.
 
-    // Don't we have to have Confirm Password field on Register form?
-    // TODO: If yes, then check if password confirmation matches password field
+    // TODO: Don't we have to have Confirm Password field on Register form?
+    // If yes, then check if password confirmation matches password field
 
-    // check that email address does not already exist in DB:
+    // Check that email address does not already exist in DB
     const dbUserCheck: QueryResult<any> = await query(
       `SELECT * 
       FROM users 
       WHERE user_email = $1`,
       [email]);
 
-    // 422 OR 400 OR 409 (resource conflict) when a record already exists.
-    //"You will receive a confirmation email on the next minutes if you don't have an account, if you don't receive the email try forget password"
     if (dbUserCheck.rows[0]) {
-      return res.status(422).send({ message: "Registration not successful." })
+      return res.status(400).send({ message: "Registration not successful." })
     }
 
+    // Once registration data checks pass, hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Create user in DB
@@ -53,10 +53,19 @@ export const registerUser = async (req: Request, res: Response, next: NextFuncti
       VALUES ($1, $2, $3) RETURNING *`,
       [username, email, hashedPassword]
     );
-    // return response
-    res.send({
+
+    const user: User = rows[0];
+    
+    // Once user is created, return success and set access token 
+    // Authorization
+    const accessToken: string = jwt.sign(user, secret);
+
+    // Return success response
+    return res.send({
       message: "User registered successfully",
-      user: rows
+      token: accessToken,
+      username: user.userName,
+      email: user.userEmail
     })
   }
   catch (error) {
@@ -65,22 +74,15 @@ export const registerUser = async (req: Request, res: Response, next: NextFuncti
 };
 
 export const loginUser = async (req: Request, res: Response, next: NextFunction) => {
-  // take user data from req.body
-  
+  // Take user data from req.body
   const { email, password } = req.body;
 
-  // check for value in name, email and password - required fields validation
+  // Check for value in name, email and password - required fields validation
   if (!email || !password) {
     return res.status(400).send({ message: "Email or password required." });
   }
 
-
-  // TODO: check for expected value in name, email and password - valid data format
-  // name: string containing letters, numbers?, special characters?
-  // email: standard email format validation a@a.a
-  // password: Your password must contain an uppercase, a lowercase, a special character and a number.
-
-  // find user in DB based on email
+  // Find user in DB based on email
   const dbUserCheck: QueryResult<any> = await query(
     `SELECT * 
     FROM users 
@@ -88,31 +90,29 @@ export const loginUser = async (req: Request, res: Response, next: NextFunction)
     [email]
   );
 
-  // if not found, return message, status 404 (Not a good practice, but leave it for now)
   const user = dbUserCheck.rows[0];
 
+  // If not found, return message, status 404 (Not a good practice, but leave it for now)
   if (!user) {
-    return res.status(404).send({ message: "User with that email not found - Incorrect credentials, please try again." })
+    return res.status(404).send({ message: "Incorrect credentials, please try again." })
   }
 
+  // If found, check for password match
   const passwordMatch: boolean = await bcrypt.compare(password, user.password);
 
-  // if found, check if password is correct (using bcrypt.compare method)
+  // If entered password doesn't match, return 401 message
   if (!passwordMatch) {
-    //  if entered password doesn't match, return 401 message
-    res.status(401).send({ message: "Password is not correct - Incorrect credentials, please try again." });
-  } else {
-    //  if entered password matches, return success and set access token with jwt.sign 
+    res.status(401).send({ message: "Incorrect credentials, please try again." });
+  }
+  else {
+    // If entered password matches, return success and set access token 
     // Authorization
     const accessToken: string = jwt.sign(user, secret);
     res.send({
       message: "User logged in successfully.",
       token: accessToken
-    })
+    });
   }
-
-
-  // return response/redirect?
 }
 
 export const editUser = (req: Request, res: Response) => {
