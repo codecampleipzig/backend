ad4fec938d2d1d97a977be7b64c8aea591e2839f
Anahita Kazem <anahita.kazem@bio.ntnu.no>
2019-12-12 14:17:02 +0100

    Set Jest test coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  coverageThreshold: {
+    global: {
+      branches: 20,
+      functions: 15,
+      lines: 30,
+    },
+  },
 };
